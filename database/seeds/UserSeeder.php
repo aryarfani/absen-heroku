402ec060bccc96d809cdcbb7b52cd6c160767577
Ary arfani <aryarfani@gmail.com>
2020-09-20 20:34:56 +0700

    edit

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
             'address' => 'Kediri, Jatim',
             'phone' => '081234123423',
             'npm' => '18.1.03.02.0144',
+            'gambar' => '1889207819.jpg',
             'password'  => bcrypt('rifqi123')
         ]);
     }
